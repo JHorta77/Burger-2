a9878268d02eb0d2468ca14331754238f38b3162
john horta <john.horta@yahoo.com>
2019-08-21 15:42:24 -0400

    forth

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-var express = require("express");
+const express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+const db = require("./models");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
 app.use(express.static("public"));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
